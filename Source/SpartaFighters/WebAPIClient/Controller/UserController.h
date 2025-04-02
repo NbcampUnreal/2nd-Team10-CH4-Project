65499f98b3df75c6c8ede8094d6204493105caab
Han jeong hyeok <gkswjdgur552@gmail.com>
2025-04-02 17:57:43 +0900

    [Add] WebAPI Client Module for Unreal

---
+++ Source/SpartaFighters/WebAPIClient/Controller/UserController.h
@@ -0,0 +1,33 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "WebAPIClient/RequestBody/RequestUser.h"
+#include "WebAPIClient/ResponseBody/ResultUser.h"
+#include "UserController.generated.h"
+
+class UWebAPIClient;
+
+UCLASS()
+class SPARTAFIGHTERS_API UUserController : public UObject
+{
+	GENERATED_BODY()
+
+public:
+    UUserController();
+
+    static UUserController* GetInstance();
+
+    void GetAllUsers(TFunction<void(TArray<FUser>)> Callback);
+    void CreateAccount(const FRequestCreateAccount& Request, TFunction<void(TSharedPtr<FResultCreateAccount>)> Callback);
+    void LogIn(const FRequestLogIn& Request, TFunction<void(TSharedPtr<FResultLogIn>)> Callback);
+
+
+private:
+    static UUserController* Instance;
+    UWebAPIClient* WebAPIClient;
+
+    FString URL = TEXT("http://localhost:5000/api/User");
+
+    //static FCriticalSection Mutex; // 멀티스레드 안전성 보장
+};
