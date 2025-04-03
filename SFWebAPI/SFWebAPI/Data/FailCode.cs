84556712386f61bad823c9d9bf1b0f1e13d13143
Han jeong hyeok <gkswjdgur552@gmail.com>
2025-04-03 09:31:31 +0900

    [Add] ResultCode Enum Class

--- SFWebAPI/SFWebAPI/Data/FailCode.cs
+++ SFWebAPI/SFWebAPI/Data/FailCode.cs
@@ -2,7 +2,7 @@
 {
     public enum FailCode
     {
-        SessionIDNotFound = 1000,        // Request 시 SessionIDfmf 못찾았을 경우.
+        SessionIDNotFound = 1000,        // Request 시 SessionID를 못찾았을 경우.
         AccountNotFound = 1001,          // 아이디를 찾을 수 없음.
         PasswordMismatched = 1002,       // 패스워드가 틀렸음.
         AccountExist = 1003,             // 동일한 아이디가 있음.
