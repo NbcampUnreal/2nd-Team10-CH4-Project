444151e095c10f00f27ca54a23780a1532f987f4
yeong132 <pfyrmog6964@gmail.com>
2025-04-04 15:03:45 +0900

    [Update] Add Code StatusContainer Component

--- Source/SpartaFighters/Character/SFCharacter.h
+++ Source/SpartaFighters/Character/SFCharacter.h
@@ -2,26 +2,34 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "Character/Stats/StatusContainerInterface.h"
 #include "SFCharacter.generated.h"
 
 class UMovementInputComponent;
+class UStatusContainerComponent;
 
 UCLASS()
-class SPARTAFIGHTERS_API ASFCharacter : public ACharacter
+class SPARTAFIGHTERS_API ASFCharacter : public ACharacter, public IStatusContainerInterface
 {
 	GENERATED_BODY()
 
 public:
 	ASFCharacter();
 
+	virtual UStatusContainerComponent* GetStatusContainerComponent() const override;
+
 protected:
 	virtual void BeginPlay() override;
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+	void InitializeCharacterProperties();
 	void Landed(const FHitResult& Hit);
 
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Input")
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "MovementInput")
 	UMovementInputComponent* MovementInputComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Stat")
+	UStatusContainerComponent* StatusContainerComponent;
 
 public:	
 	virtual void Tick(float DeltaTime) override;
