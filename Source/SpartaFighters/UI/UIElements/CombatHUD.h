8257e09c4f55851babd1c5f68a0520b30846ebc3
Han jeong hyeok <gkswjdgur552@gmail.com>
2025-04-15 19:05:17 +0900

    [Add] Show Current HP Progress bar ( 현재 내 체력의 변화만 표시되고 있음 수정필요 )

--- Source/SpartaFighters/UI/UIElements/CombatHUD.h
+++ Source/SpartaFighters/UI/UIElements/CombatHUD.h
@@ -5,10 +5,28 @@
 #include "CombatHUD.generated.h"
 
 class UUniformGridPanel;
+class UCharacterStatusWidget;
+class UHorizontalBox;
 
 UCLASS()
 class SPARTAFIGHTERS_API UCombatHUD : public UBaseUserWidget
 {
 	GENERATED_BODY()
 
+public:
+	void NativeConstruct() override;
+
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<UCharacterStatusWidget> CharacterStatusWidget;
+
+	UPROPERTY(meta = (BindWidget))
+	UHorizontalBox* StatusContainer;
+
+	void CreateCharacterStatusWidget();
+
+	UFUNCTION()
+	void RebindCharacterStatus(APawn* NewPawn);
+
+private:
+	FTimerHandle InitCreateWidgetTimer;
 };
