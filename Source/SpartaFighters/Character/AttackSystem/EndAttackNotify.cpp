61eaad1808e875245fe9a4378cd6974df0cfe591
Han jeong hyeok <gkswjdgur552@gmail.com>
2025-04-16 05:18:45 +0900

    [Add] EndAttack Notify

---
+++ Source/SpartaFighters/Character/AttackSystem/EndAttackNotify.cpp
@@ -0,0 +1,20 @@
+#include "Character/AttackSystem/EndAttackNotify.h"
+#include "Character/SFCharacter.h"
+#include "Character/Components/SkillComponent.h"
+
+void UEndAttackNotify::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation)
+{
+	if (MeshComp && MeshComp->GetOwner())
+	{
+		AActor* Owner = MeshComp->GetOwner();
+		ASFCharacter* Character = Cast<ASFCharacter>(Owner);
+
+		if (Character)
+		{
+			if (Character->SkillComponent)
+			{
+				Character->SkillComponent->OnMontageEnded();
+			}
+		}
+	}
+}
